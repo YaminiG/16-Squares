4f4130930622ec59073826cc5ef0c680bac1c246
thesourabh <sourabhshetty@gmail.com>
2018-03-18 21:16:43 -0400

    rotatable black block over 100rs note

---
+++ Assets/TestRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour {
+    float rotSpeed = 500;
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        // float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+        // transform.Rotate(Vector3.right, -rotX);
+    }
+
+
+    /*void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            float rotX = rotSpeed * Mathf.Deg2Rad;
+            float rotY = rotSpeed * Mathf.Deg2Rad;
+            transform.
+
+            transform.Rotate(Vector3.up, -rotX);
+            transform.Rotate(Vector3.right, rotY);
+        }
+    }*/
+}
