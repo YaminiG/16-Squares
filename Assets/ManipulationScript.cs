0943b057d8a98d1b9af9e023474f491d2e33a6e7
thesourabh <sourabhshetty@gmail.com>
2018-03-25 20:22:47 -0400

    unbroke rotation by adding a box collider to the object

--- Assets/ManipulationScript.cs
+++ Assets/ManipulationScript.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ManipulationScript : MonoBehaviour {
 
-
-    float rotSpeed = 20;
+    float rotSpeed = 200;
 
     private void OnMouseDrag()
     {
         float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+        //float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
 
         transform.Rotate(Vector3.up, -rotX);
-        transform.Rotate(Vector3.right, -rotY);
+        //transform.Rotate(Vector3.right, -rotY);
     }
 }
