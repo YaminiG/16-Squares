fb307984dbf2bf2bd4b4d1b3fc2075124a13ed03
thesourabh <sourabhshetty@gmail.com>
2018-04-04 22:06:51 -0400

    freshen up the project from scratch

---
+++ Assets/ModelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelScript : MonoBehaviour
+{
+
+    float rotSpeed = 200;
+
+    // Rotation
+    private void OnMouseDrag()
+    {
+        Debug.Log("ON MOUSE DRAG");
+
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        //float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+        //transform.Rotate(Vector3.right, -rotY);
+
+    }
+
+}
